7e3980c8dc80d07e7343c7c5a2cded918b8e0a98
heeYeon kim <wow9896@naver.com>
2019-07-10 13:49:14 +0900

    Android studio practice

---
+++ example1/app/src/main/java/com/ourincheon/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ourincheon.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Textview.text="버튼을 눌렀습니다"
+        }
+    }
+
+}
